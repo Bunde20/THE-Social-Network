5a323756659d8f7ac315266bde543202df57244a
Steven Bunde <stevenbunde2020@gmail.com>
2023-11-06 18:03:17 -0500

    worked on the routes and models for thoughts/users Still need to complete them

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -29,4 +29,64 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// POST a new thought and push the created thought's _id to the associated user's thoughts array field
+router.post('/', (req, res) => {
+    Thought.create(req.body)
+    .then((dbThoughtData) => {
+        return User.findOneAndUpdate(
+            { username: req.body.username },
+            { $push: { thoughts: dbThoughtData._id } },
+            { new: true }
+        );
+    })
+    .then(dbUserData => {
+        // If no user is found, send 404
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => res.status(400).json(err));
+});
+
+// PUT to update a thought by its _id
+router.put('/:id', (req, res) => {
+    Thought.findOneAndUpdate (
+        { _id: req.params.id },
+        { $set: req.body },
+        { runValidators: true, new: true }
+    )
+    .then(dbThoughtData => {
+        // If no thought is found, send 404
+        if (!dbThoughtData) {
+            res.status(404).json({ message: 'No thought found with this id!' });
+            return;
+        }
+        res.json(dbThoughtData);
+    })
+    .catch(err => res.status(400).json(err));
+});
+
+// DELETE to remove a thought by its _id
+router.delete('/:id', (req, res) => {
+    Thought.findOneAndDelete({ _id: req.params.id })
+    .then((dbThoughtData) => {
+        return User.findOneAndUpdate(
+            { username: dbThoughtData.username },
+            { $pull: { thoughts: dbThoughtData._id } },
+            { new: true }
+        );
+    })
+    .then(dbUserData => {
+        // If no user is found, send 404
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+        res.json({ message: 'Thought and associated user deleted!' });
+    })
+    .catch(err => res.status(400).json(err));
+});
+
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -28,4 +28,108 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// POST a new user
+router.post('/', (req, res) => {
+    User.create(req.body)
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => res.status(400).json(err));
+});
+
+// PUT one friend into a user's friend list
+router.put('/:userId/friends/:friendId', (req, res) => {
+    User.findOneAndUpdate (
+        { _id: req.params.userId },
+        { $push: { friends: req.params.friendId } },
+        { new: true }
+    )
+    .then(dbUserData => {
+        // If no user is found, send 404
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => res.status(400).json(err));
+});
+
+// PUT to update a user by its _id
+router.put('/:id', (req, res) => {
+    User.findOneAndUpdate (
+        { _id: req.params.id },
+        { $set: req.body },
+        { new: true }
+    )
+    .then(dbUserData => {
+        // If no user is found, send 404
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => res.status(400).json(err));
+});
+
+// DELETE user by its _id
+router.delete('/:id', (req, res) => {
+    User.findOneAndDelete({ _id: req.params.id })
+    .then(dbUserData => {
+        // If no user is found, send 404
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+        // Remove the user from any friend arrays
+        User.updateMany(
+            { friends: req.params.id },
+            { $pull: { friends: req.params.id } }
+        )
+        .then(() => {
+            // Delete any thoughts associated with the user
+            Thought.deleteMany({ username: dbUserData.username })
+            .then(() => {
+                res.json({ message: 'Successfully deleted user!' });
+            })
+            .catch(err => res.status(400).json(err));
+        })
+        .catch(err => res.status(400).json(err));
+    })
+    .catch(err => res.status(400).json(err));
+});
+
+// DELETE one friend from a user's friend list
+router.delete('/:userId/friends/:friendId', (req, res) => {
+    User.findOneAndUpdate (
+        { _id: req.params.userId },
+        { $pull: { friends: req.params.friendId } },
+        { new: true }
+    )
+    .then(dbUserData => {
+        // If no user is found, send 404
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+        // Remove the user from any friend arrays
+        User.updateMany(
+            { friends: req.params.id },
+            { $pull: { friends: req.params.id } }
+        )
+        .then(() => {
+            // Delete any thoughts associated with the user
+            Thought.deleteMany({ username: dbUserData.username })
+            .then(() => {
+                res.json({ message: 'Successfully deleted user!' });
+            })
+            .catch(err => res.status(400).json(err));
+        })
+        .catch(err => res.status(400).json(err));
+        res.json(dbUserData);
+    })
+    .catch(err => res.status(400).json(err));
+});
+
+
+
 module.exports = router;
